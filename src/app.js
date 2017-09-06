d3cb9c41298a64ac7b3376c7f65b4432a3bf2912
Rodrigo Castro <rfocastro@sp.r7.com>
2017-09-06 18:49:19 -0300

    criando controllers

--- src/app.js
+++ src/app.js
@@ -6,36 +6,16 @@ const bodyParser = require ('body-parser'); //tratar as requisiçõe
 const app    = express();
 const router = express.Router(); 
 
+// Caarrega as rotas
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const route = router.get('/', (req, res, next) => {
-  res.status(200).send({
-    title: 'Nodse Store Api',
-    version: '0.0.1'
-  });
-});
 
-const create = router.post('/', (req, res, next) => {
-  res.status(201).send(req.body);
-});
-
-const put = router.put('/:id', (req, res, next) => {
-  let id = req.params.id;
-  res.status(201).send({
-    id: id, 
-    item: req.body
-  });
-});
-
-const exclude = router.delete('/', (req, res, next) => {
-  res.status(201).send(req.body);
-});
-
-app.use('/', route);
-app.use('/products', create);
-app.use('/products', put);
-app.use('/products', exclude);
+app.use('/', indexRoute);
+app.use('/products', productRoute);
 
 module.exports = app;
 
